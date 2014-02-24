39364fe04c1f1c3ec3e97f811691426136cbb41c
Christoph Schniedermeier <zdxerr@gmail.com>
2014-02-24 16:31:08 +0100

    Some buttons and paned.

--- testrun/gui.py
+++ testrun/gui.py
@@ -1,44 +1,11 @@
-from tkinter import *
-from tkinter import ttk
+"""
+"""
 
-# def calculate(*args):
-#     try:
-#         value = float(feet.get())
-#         meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
-#     except ValueError:
-#         pass
-    
-# root = Tk()
-# root.title("Teststep")
+from pprint import pprint
 
-# mainframe = ttk.Frame(root, padding="3 3 12 12")
-# mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-# mainframe.columnconfigure(0, weight=1)
-# mainframe.rowconfigure(0, weight=1)
-
-# feet = StringVar()
-# meters = StringVar()
-
-# feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-# feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-# ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-# ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-# ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-# ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-# ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-# for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-# feet_entry.focus()
-# root.bind('<Return>', calculate)
-
-# root.mainloop()
-import tkinter
+import tkinter as tk
 
 class HyperlinkManager:
-
     def __init__(self, text):
         self.text = text
 
@@ -67,74 +34,116 @@ class HyperlinkManager:
         self.text.config(cursor="")
 
     def _click(self, event):
-        for tag in self.text.tag_names(CURRENT):
+        for tag in self.text.tag_names(tk.CURRENT):
             if tag[:6] == "hyper-":
                 self.links[tag]()
                 return
 
-class App:
-    def __init__(self, root):
-        self.root = root
 
-        frame = Frame(root, bd=2, relief=SUNKEN)
+class ScrolledText(tk.Text):
+    def __init__(self, master, name, **kwargs):
+        self.frame = tk.LabelFrame(master, text=name, borderwidth=0)
 
-        xscrollbar = Scrollbar(frame, orient=HORIZONTAL)
-        xscrollbar.pack(side=BOTTOM, fill=X)
+        x_scrollbar = tk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
+        x_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
 
-        yscrollbar = Scrollbar(frame)
-        yscrollbar.pack(side=RIGHT, fill=Y)
+        y_scrollbar = tk.Scrollbar(self.frame)
+        y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
+        super(ScrolledText, self).__init__(self.frame, 
+                         xscrollcommand=x_scrollbar.set, 
+                         yscrollcommand=y_scrollbar.set,
+                         **kwargs)
 
-        text = tkinter.Text(frame, width=80, height=30, 
-                            font=("Ubuntu", 13),
-                            wrap=NONE,
-                            xscrollcommand=xscrollbar.set,
-                            yscrollcommand=yscrollbar.set)
-        text.pack()
-        frame.pack()
+        x_scrollbar.config(command=self.xview)
+        y_scrollbar.config(command=self.yview)
 
-        xscrollbar.config(command=text.xview)
-        yscrollbar.config(command=text.yview)
+        self.pack(expand=True, fill=tk.BOTH)
 
-        hyperlink = HyperlinkManager(text)
-
-        text.insert(INSERT, "this is a ")
-        text.insert(INSERT, "link", hyperlink.add(self.click))
-        text.insert(INSERT, "\n\n")
-
-        text.tag_config("code", font=("SourceCodePro", 13), background='lightgray')
-        text.tag_config("bold", font=("Ubuntu", 13, "bold"))
-        text.tag_config("italic", font=("Ubuntu", 13, "italic"))
-        text.insert(END, "HELLO\n\n", ("bold"))
-        text.insert(END, "HELLO\n\n", ("italic"))
-        text.insert(END, "BOTH\n\n", ("bold", "italic"))
-
-        text.tag_config('block1', lmargin1=15)
-        text.tag_config('block2', lmargin1=25)
-        text.tag_config('block3', lmargin1=35)
-
-        text.insert(END, "line1\n", ("block1"))
-        text.insert(END, "line2\n", ("block2"))
-        text.insert(END, "line3\n", ("block3"))
+        self.links = HyperlinkManager(self)
 
 
-        code_string = """# For loop on a list
->>> list = [2, 4, 6, 8]
->>> sum = 0
->>> for num in list:
->>>     sum = sum + num
->>> print("The sum is:", sum)
-The sum is: 20"""
-        text.insert(END, code_string, ("code"))
 
-        for text in ("link1", "link2", "link3"):
-            link = tkinter.Label(text=text, foreground="#0000ff")
-            link.bind("<1>", lambda event, text=text: \
-                          self.click_link(event, text))
-            link.pack()
+    def pack(self, *args, **kwargs):
+        super(ScrolledText, self).pack(expand=True, fill=tk.BOTH)
+        self.frame.pack(*args, **kwargs)
 
-    def click(self):
-        print("CLICK!")
+def click(self):
+    print("CLICK!")
+
+def add_text_example(question):
+    question.insert(tk.INSERT, "this is a ")
+    question.insert(tk.INSERT, "link", question.links.add(click))
+    question.insert(tk.INSERT, "\n\n")
+
+    question.tag_config("code", font=("Consolas", 13), background='lightgray')
+    question.tag_config("bold", font=("Ubuntu", 13, "bold"))
+    question.tag_config("italic", font=("Ubuntu", 13, "italic"))
+    question.insert(tk.END, "HELLO\n\n", ("bold"))
+    question.insert(tk.END, "HELLO\n\n", ("italic"))
+    question.insert(tk.END, "BOTH\n\n", ("bold", "italic"))
+
+    question.tag_config('block1', lmargin1=15)
+    question.tag_config('block2', lmargin1=25)
+    question.tag_config('block3', lmargin1=35)
+
+    question.insert(tk.END, "line1\n", ("block1"))
+    question.insert(tk.END, "line2\n", ("block2"))
+    question.insert(tk.END, "line3\n", ("block3"))
+
+
+    code_string = '\n'.join([
+        '# For loop on a list',
+        '>>> list = [2, 4, 6, 8]',
+        '>>> sum = 0',
+        '>>> for num in list:',
+        '>>>     sum = sum + num',
+        '>>> print("The sum is:", sum)',
+        'The sum is: 20',
+        ])
+    question.insert(tk.END, code_string, ("code"))
+
+    question.config(state=tk.DISABLED)
+
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super(App, self).__init__(*args, **kwargs)
+
+        # always on top
+        self.wm_attributes("-topmost", 1)
+
+        self.title("Teststep")
+
+        paned = tk.PanedWindow(self, orient=tk.VERTICAL, sashpad=3, 
+                               sashwidth=3, sashrelief=tk.RAISED)
+        paned.pack(expand=True, fill=tk.BOTH, pady=3)
+        pprint(paned.keys())
+
+        task = ScrolledText(paned, 'Task', width=80, font=("Calibri", 14), 
+                                wrap=tk.NONE)
+        paned.add(task.frame)
+        # task.pack(expand=True, fill=tk.BOTH)
+
+        add_text_example(task)
+        
+        comment = ScrolledText(self, 'Comment', width=80, height=5, font=("Calibri", 14), wrap=tk.NONE)
+        paned.add(comment.frame)
+
+        frame_buttons = tk.Frame(self, pady=3)
+
+        pprint(frame_buttons.keys())
+
+        button_passed = tk.Button(frame_buttons, text="Passed", height=1, command=click)
+        button_passed.pack(expand=True, fill=tk.X, side=tk.LEFT, padx=3)
+        button_failed = tk.Button(frame_buttons, text="Failed", width=12, height=1, command=click)
+        button_failed.pack(expand=True, fill=tk.X, side=tk.LEFT, padx=3)
+        button_blocked = tk.Button(frame_buttons, text="Blocked", width=12, height=1, command=click)
+        button_blocked.pack(expand=True, fill=tk.X, side=tk.LEFT, padx=3)
+
+
+        frame_buttons.pack(expand=True, fill=tk.X)
+
+    
 
     def click_link(self, event, text):
         print("you clicked '%s'" % text)
@@ -148,6 +157,7 @@ def question(func):
         return func(*args, **kwargs) #2
     return inner
 
+
 @question
 def set_task_configuration(test):
     """
@@ -155,9 +165,11 @@ def set_task_configuration(test):
     """
     pass
 
-root=tkinter.Tk()
-app = App(root)
-root.mainloop()
+
+if __name__ == '__main__':
+    # root=tkinter.Tk()
+    app = App()
+    app.mainloop()
 
 
 
